d3c550c00e0bb86231b98f61415edab93155e289
Karolos Triantafyllou <karolos.triantafyllou@gmail.com>
2021-11-19 10:37:05 +0200

    style: Minor fixes

--- bin/.local/bin/wallpaper.py
+++ bin/.local/bin/wallpaper.py
@@ -6,7 +6,7 @@ import random
 import time
 
 # DEFAULT FOLDER
-FOLDER_WALL = '/home/charlie/Pictures/Wallpapers/1920x1080/'
+FOLDER_WALL = "/home/charlie/Pictures/Wallpapers/1920x1080/"
 
 def interval_wall(wallpapers, seconds):
     while True:
@@ -18,19 +18,25 @@ def interval_wall(wallpapers, seconds):
             wallpapers = glob.glob(FOLDER_WALL + "*")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Set random wallpapers")
-    parser.add_argument('--version', action='version', version='%(prog)s 0.1.0')
-    parser.add_argument('-d', '--directory', type=str, nargs=1, help='specific the main folder')
-    parser.add_argument('-t', '--time', type=int, nargs=1, help='interval of background changes')
+    parser.add_argument(
+        "--version", action="version", version="%(prog)s 0.1.0"
+    )
+    parser.add_argument(
+        "-d", "--directory", type=str, nargs=1, help="specific the main folder"
+    )
+    parser.add_argument(
+        "-t", "--time", type=int, nargs=1, help="interval of background changes"
+    )
     args = parser.parse_args()
 
     if args.directory:
         FOLDER_WALL = args.directory[0]
 
-    wallpapers = glob.glob(FOLDER_WALL + "*")
+        wallpapers = glob.glob(FOLDER_WALL + "*")
 
-    if args.time:
-        interval_wall(wallpapers, args.time[0])
-    else:
+        if args.time:
+            interval_wall(wallpapers, args.time[0])
+else:
         print(random.choice(wallpapers))
